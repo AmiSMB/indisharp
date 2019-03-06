b3ded5f4e74cfe5acdbaf5d433ee20526914694a
mmking2048 <myang22@otsads.iit.edu>
2019-03-05 19:20:45 -0600

    Added launch point for winforms. Note that it doesn't actually work.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using INDI.Forms;
+
+namespace INDI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new INDIForm(new INDIClient("burnterwaffle.freemyip.com", 7624)));
+        }
+    }
+}
